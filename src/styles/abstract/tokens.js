f2bd0fd1a9a4e39e9e7c083d2bce5d25d3fcbb78
Dillon Schultz <dillonschultz93@gmail.com>
2020-12-06 13:34:42 -0700

    Add another hue to the color tokens

--- src/styles/abstract/tokens.js
+++ src/styles/abstract/tokens.js
@@ -57,6 +57,7 @@ export default css`
   --color-danger-2: rgba(255,80,68,1);
   --color-danger-3: rgba(230,72,61,1);
   --color-danger-4: rgba(202,63,53,1);
+  --color-danger-5: rgba(167,52,44,1);
 
   --color-info-0: rgba(150,196,255,1);
   --color-info-1: rgba(114,175,255,1);
