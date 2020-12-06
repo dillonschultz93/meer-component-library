1e5796a3812588ae629614d05cfcf4ab368ee7ca
Dillon Schultz <dillonschultz93@gmail.com>
2020-12-06 13:35:20 -0700

    Add playwright to the test runner

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,4 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
 export default {
   files: 'test/**/*.test.js',
+  browser: [
+    playwrightLauncher({ product: 'chromium' }),
+    playwrightLauncher({ product: 'firefox' }),
+    playwrightLauncher({ product: 'webkit' }),
+  ],
+  concurrency: 10,
   nodeResolve: true,
 };
